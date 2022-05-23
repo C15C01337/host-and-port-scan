8dcf04ca39af775f1ee4985263d9003a0761ebc9
Bishal Aryal <70318933+C15C01337@users.noreply.github.com>
2022-05-23 11:34:43 +0000

    Update and rename network.bash to host-and-ping-scan.sh

--- host-and-ping-scan.sh
+++ host-and-ping-scan.sh
@@ -1,18 +1,12 @@
 echo "[+]Enter the type of scan 1) Host-Scan 2) Ping Scan "
 read file
 if [ "$file" == "1" ]; then
-    
     echo "[+]Enter IP address: "
     read IP
-    
     echo "[+]Enter Subnet mask address: "
     read subnet
-    
     nmap -sp $IP/$subnet -oG nmap_output
-	
-    
 fi
-
 
 echo "[+]Enter the name of file if available: "
 read fileName
@@ -22,17 +16,11 @@ do
     echo "$line"
 done < "$input"
 
-if [ "$file" == "2" ]; then
-    
-    
+if [ "$file" == "2" ]; then    
     nmap -sn $line/$subnet -oG nmap_output2
-    
 else
     echo "[+]Enter the ip address: "
     read ipAddress
-    
     nmap -sn $ipAddress/$subnet -oG nmap_output1
 fi
-
-
 
